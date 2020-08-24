6cca149cecd8d43d186eb8d11d4d124f864429f8
Ted Morin <morinted@gmail.com>
2020-08-24 10:18:22 -0400

    Planck steno and game

---
+++ keyboards/planck/keymaps/morinted/rules.mk
@@ -0,0 +1,7 @@
+SRC += muse.c
+EXTRAKEY_ENABLE = no
+MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
+STENO_ENABLE = yes          # Additional protocols for Stenography(+1700), requires VIRTSER
+AUDIO_ENABLE = yes           # Audio output on port C6
+MIDI_ENABLE = no            # MIDI controls
+RGBLIGHT_SLEEP = yes
